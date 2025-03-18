bdfa198c2412d5679610d0f01575d99f0863b5c8
Jogeshwar Singh <singhjogeshwar007@gmail.com>
2025-03-18 19:56:34 +0530

    chore: mobile trading coming soon

--- frontend/src/pages/Trade.tsx
+++ frontend/src/pages/Trade.tsx
@@ -29,16 +29,24 @@ export const Trade = () => {
         <link rel="icon" href="/favicon.svg" type="image/x-icon" />
       </Helmet>
 
-      <div className="flex items-center justify-between border-b border-border px-8 py-2.5">
+      <div className="flex flex-col gap-8 lg:hidden uppercase h-[100vh] text-2xl font-mono w-full justify-center items-center">
+        <div className="flex gap-2 items-center">
+          <img src="/logo.svg" alt="Vest" className="h-8 mr-2" />
+          <img src="/vest.svg" alt="Vest" className="h-6" />
+        </div>
+        <div>Mobile Trading Coming soon</div>
+      </div>
+
+      <div className="hidden lg:flex items-center justify-between border-b border-border px-8 py-2.5">
         <PromoteBar />
       </div>
 
-      <header className="hidden h-15 shrink-0 grid-cols-5 border-b border-border bg-background md:flex">
+      <header className="hidden h-15 shrink-0 grid-cols-5 border-b border-border bg-background lg:flex">
         <NavBar />
       </header>
 
       <main className="h-full">
-        <div className="hidden h-full md:block">
+        <div className="hidden h-full lg:block">
           <div className="flex h-full flex-col">
             <div className="grid h-[56px] shrink-0 grid-cols-5 border-b border-border bg-background">
               <MarketBar market={market as string} />
